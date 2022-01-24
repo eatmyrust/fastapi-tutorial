6bcffd19f392ccdc0b4296f6e683b4b5854400ac
Dylan Jenik <dylanjenikcodes@gmail.com>
2022-01-23 20:44:39 -0500

    Add pydantic models to only return necessary fields to user upon user creation

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,7 +1,16 @@
 from typing import Optional
+from xmlrpc.client import boolean
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
     username : str
     email : EmailStr
     password: str
+
+class ShowUser(BaseModel):
+    username : str
+    email : EmailStr
+    is_active : bool
+
+    class Config():
+        orm_mode = True

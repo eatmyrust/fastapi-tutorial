afbfb578d45aef596fde5c0f3e2756a346aacf58
Dylan Jenik <dylanjenikcodes@gmail.com>
2022-02-06 16:22:16 -0500

    Add functionality to show list of all jobs on homepage, with each job having a card

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from db.repository.jobs import list_jobs
+from sqlalchemy.orm import Session
+from db.session import get_db
+from fastapi import Depends
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(include_in_schema=False)
 
 @router.get("/")
-def home(request: Request):
-    print(dir(request))
-    return templates.TemplateResponse("jobs/homepage.html", {"request":request})
+def home(request: Request, db:Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/homepage.html", {"request":request, "jobs":jobs})
